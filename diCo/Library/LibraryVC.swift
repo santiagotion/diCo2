1abe917ebd1e9c6a99c386f9680b59b855959c34
Nathan Tuala <ntuala1@stu.parkland.edu>
2021-01-15 14:29:01 -0600

    hhh

--- diCo/Library/LibraryVC.swift
+++ diCo/Library/LibraryVC.swift
@@ -12,7 +12,13 @@ class LibraryVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
     var wordToPass: Word!
+    var isSearch: Bool = false
+    let searchController = UISearchController(searchResultsController: nil)
+    var filteredWords : [Word] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,26 +26,84 @@ class LibraryVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
         if #available(iOS 13.0, *) {
             // searchField.overrideUserInterfaceStyle = .light
         }
+        //self.myButton.isHidden = true
         tableView.delegate = self
+        tableView.allowsSelectionDuringEditing = true
+        tableView.allowsSelection = true
+        
+        if #available(iOS 14.0, *) {
+            tableView.selectionFollowsFocus = true
+        } else {
+            // Fallback on earlier versions
+        }
         // Do any additional setup after loading the view.
+        
+        // 1
+        searchController.searchResultsUpdater = self
+        // 2
+        searchController.obscuresBackgroundDuringPresentation = false
+    
+        // 3
+        searchController.searchBar.placeholder = "Search..."
+        // 4
+        navigationItem.searchController = searchController
+        // 5
+        definesPresentationContext = true
+        
+        //tableView.tableHeaderView = searchController.searchBar
+        
+        searchBar.delegate = self
+        
+        //searchBar.showsCancelButton = true
+
+    }
+    var isSearchBarEmpty: Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func filterContentForSearchText (_ searchText: String) {
+        filteredWords = DataHolder.dict_data.filter { (word: Word) -> Bool in
+            return word.word.lowercased().contains(searchText.lowercased())
+        }
+        if filteredWords.count==0 {
+            //self.myButton.isHidden = false
+        }
+        tableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        wordToPass = DataHolder.dict_data[indexPath.item]
+        //wordToPass = DataHolder.dict_data[indexPath.item]
+        if isSearch {
+            wordToPass = filteredWords[indexPath.row]
+        }else {
+            wordToPass = DataHolder.dict_data[indexPath.row]
+        }
         self.performSegue(withIdentifier: "toDetails1", sender: indexPath)
         tableView.delegate = self
         
     }
     
+    var isFiltering: Bool {
+        return searchController.isActive && !isSearchBarEmpty
+    }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isSearch {
+            return filteredWords.count
+        }
         return DataHolder.dict_data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "popularCell", for: indexPath) as! LibraryCell
-        cell.setMeaning(dictionary: DataHolder.dict_data[indexPath.row])
+        let word:Word
+        if isSearch {
+            word = filteredWords[indexPath.row]
+        }else {
+            word = DataHolder.dict_data[indexPath.row]
+        }
+        cell.setMeaning(dictionary: word)
         cell.delegate = self
         return cell
     }
@@ -56,5 +120,40 @@ class LibraryVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 extension LibraryVC: TableViewDelegate{
     func playSound(url: String) {
         PlaySound.playSound(url: url)
+    }
+}
+
+extension LibraryVC: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+}
+
+extension LibraryVC : UISearchBarDelegate{
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        isSearch = true;
+    }
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        isSearch = false
+        searchBar.resignFirstResponder()
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearch = false
+        searchBar.resignFirstResponder()
+        tableView.reloadData()
+    }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        isSearch = false;
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if (searchText.isEmpty) {
+            isSearch = false
+            tableView.reloadData()
+        }else {
+            isSearch = true
+            filterContentForSearchText (searchText)
+        }
     }
 }
